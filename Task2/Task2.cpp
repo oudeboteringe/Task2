31604135fcfd61605cb70699bc34457be549d03b
oudeboteringe <michiel_72@hotmail.com>
2017-01-08 09:35:21 +0100

    Minor

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -3,9 +3,37 @@
 
 #include "stdafx.h"
 
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+#include "Solution2.h"
 
 int main()
 {
-    return 0;
+
+  // Declare the vector:
+  vector<int> intVec;
+
+  intVec.push_back(-1);
+  intVec.push_back(3);
+  intVec.push_back(-4);
+  intVec.push_back(5);
+  intVec.push_back(1);
+  intVec.push_back(-6);
+  intVec.push_back(2);
+  intVec.push_back(1);
+  intVec.push_back(1);
+  intVec.push_back(-6);
+
+  int result = solution(intVec);
+  cout << "Result: " << result << endl;
+
+  // End of program
+  cout << "Press any key to exit.";
+  _getch();
+
+  return 0;
 }
+
 
